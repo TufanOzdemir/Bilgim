4e729df23c92a19f4c36b91cedf98f19885d88c6
Batuhan <yahsibatuhan@gmail.com>
2018-05-03 11:48:42 +0300

    #First Custom renderer added.

---
+++ QuizTest/QuizTest.Android/CustomRenderers/CustomFlatButtonRenderer.cs
@@ -0,0 +1,28 @@
+﻿using Android.Content;
+using QuizTest.CustomComponents;
+using QuizTest.Droid.CustomRenderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomFlatButton), typeof(CustomFlatButtonRenderer))]
+namespace QuizTest.Droid.CustomRenderers
+{
+    public class CustomFlatButtonRenderer : ButtonRenderer
+    {
+        public CustomFlatButtonRenderer(Context context) : base(context)
+        {
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.SetBackgroundColor(Android.Graphics.Color.ParseColor("#ffffff"));
+                Control.SetTextColor(Android.Graphics.Color.ParseColor("#ffffff"));
+                //
+
+            }
+        }
+    }
+}
---
+++ QuizTest/QuizTest/Extensions/ViewExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizTest.Extensions
+{
+    public static class ViewExtensions
+    {
+    }
+}
